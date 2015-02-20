cfa9b6c10ea38cef9e93c0ffe0f88dbe317cf1f0
bhatshreenidhi <bhatshreenidhi@gmail.com>
2015-02-20 14:50:30 -0800

    Changes made to return two lists - 1st giving n-grams and 2nd as a helper list for n-gram tree building

--- src/ngramsEngine.py
+++ src/ngramsEngine.py
@@ -9,29 +9,50 @@ class ngramsEngine(object):
 	# Module to print n-grams
 	def printNGrams(self,ngramsList):
 		for token in ngramsList:
-			sen = ''
-			for word in token:
-				sen = sen + ' '+ word
-			print(sen.strip())
+			print(token.strip())
 
 
-	# Module that generates n-grams
+	# Module that generates n-grams list
 	# Input : query
-	# Output : list containing n-grams 
+	# Output : Two lists are returned.
+	# 1st list : This has all the n-grams arranged hierarchically
+	# 2nd list : This is a list that as list of n-grams grouped together based on the length
+	# EX:  i/p - a b c d 
+	# List1 : ['a b c d', 'a b c', 'b c d', 'a b', 'b c', 'c d', 'a', 'b', 'c', 'd']
+	# List2 : [['a', 'b', 'c', 'd'], ['a b', 'b c', 'c d'], ['a b c', 'b c d'], ['a b c d']]
+
 	def generateNGrams(self,query):
 
-		ngramsList = []
+		ngramsNLTKList = []
 		for n in range(len(query),0,-1):
-			ngramsList.extend(ngrams(query.split(),n))
-		return ngramsList
+			ngramsNLTKList.extend(ngrams(query.split(),n))
 
+		lookupList = []
+		ngramList = []
+
+
+		for ngram in ngramsNLTKList:
+			ngramList.append((' '.join(ngram)).strip())
+
+		if(len(ngramsNLTKList)>0):
+			maxLength = len(ngramsNLTKList[0])
+			for i in range(maxLength):
+				lookupList.append([])
+
+		for token in ngramsNLTKList:
+			joinedToken = ' '.join(token).strip()
+			listLength = len(joinedToken)
+			currentList = lookupList[len(token)-1]
+			currentList.append(' '.join(token))
+
+		return ngramList,lookupList
 
 
 def main():
 	ngramsEngineObject = ngramsEngine()
 	query = input(" Enter the query : ")
 
-	ngramsList = ngramsEngineObject.generateNGrams(query.strip())
+	ngramsList,lookupList = ngramsEngineObject.generateNGrams(query.strip())
 	ngramsEngineObject.printNGrams(ngramsList)
 
 if __name__ == '__main__':
