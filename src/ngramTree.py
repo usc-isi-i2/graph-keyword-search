5d77627d6e7a35e364ca1cc8d48a54cdb2094dd6
bhatshreenidhi <bhatshreenidhi@gmail.com>
2015-02-20 15:39:46 -0800

    Comments and algorithm added

--- src/ngramTree.py
+++ src/ngramTree.py
@@ -1,7 +1,7 @@
 from ngramsEngine import ngramsEngine
 
-#This class represents Node of the tree
-#A Node has value/data and also has links to its children stored as a list
+# This class represents Node of the tree
+# A Node has value/data and also has links to its children stored as a list
 class Node(object):
 
     def __init__(self, data):
@@ -13,20 +13,39 @@ class Node(object):
         self.children.append(obj)
 
 
-#This represents a n-gram tree
+# This represents a n-gram tree
 class NgramTree(object):
 
 	def __init__(self,rootNode):
 		self.rootNode = rootNode
 
-	#This module builds the n-gram tree with the basic idea of BFS traversal
+	# This module builds the n-gram tree with the basic idea of BFS traversal
+	# Input : List1, List2
+	# List1 : ['a b c d', 'a b c', 'b c d', 'a b', 'b c', 'c d', 'a', 'b', 'c', 'd']
+	# List2 : [['a', 'b', 'c', 'd'], ['a b', 'b c', 'c d'], ['a b c', 'b c d'], ['a b c d']]
+	# Algorithm :
+	# while(queue):
+	#	CurrentNode = queue.pop()
+	#	search for tokens that have a length = length(CurrentNode.value) - 1 
+	#	##[Get this from List2]
+	#	##[These tokens will be the nodes at the next level in the tree]
+	#	check if the tokens are already in the tree
+	#	if not create new node with these tokens 
+	#	add the token nodes as the children of CurrentNode
+
 	def constructTree(self,listNgrams,lookupList):
 		
+		# This dictionary is used to track the nodes that are in the tree
+		# key:Nodevalue  value:Node
 		treeDictionary = {}
+
+		# This list exhibits behaviour of a Queue
 		nodeQueue = []
 
+		#Add the root node to the Queue to begin search
 		nodeQueue.append(self.rootNode)
 		treeDictionary[self.rootNode.data] = self.rootNode
+
 
 		while(nodeQueue):
 			currentNode = nodeQueue.pop(0)
