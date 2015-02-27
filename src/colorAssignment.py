3a44682e40510822a9739b206e1713f7390c8f14
bhatshreenidhi <bhatshreenidhi@gmail.com>
2015-02-27 13:48:32 -0800

    Moved the colorAssignment dictionary to the class level so that it can be accessed using the object

--- src/colorAssignment.py
+++ src/colorAssignment.py
@@ -3,18 +3,17 @@ from ngramTree import *
 # This class is responsible for assignment of colors to the nodes in the ngram tree
 class ColorAssignment:
 
+	colorDictionary = {}	# This dictionary stores the individual tokens ['a', 'b', 'c', 'd'] and their color values
 
 	# lookuplist - [['a', 'b', 'c', 'd'], ['a b', 'b c', 'c d'], ['a b c', 'b c d'], ['a b c d']]
 	def assignInitialColors(self,rootNode,lookupList):
-		
-		colorDictionary = {}	# This dictionary stores the individual tokens ['a', 'b', 'c', 'd'] and their color values
-		
+			
 		if rootNode and len(lookupList)>=1:
 			oneGrams = lookupList[0]	# Gets the one grams		
 			
 			for index in range(len(oneGrams)):	
-				if(oneGrams[index] not in colorDictionary):
-					colorDictionary[oneGrams[index]] = index		# This assigns the color values to each token
+				if(oneGrams[index] not in self.colorDictionary):
+					self.colorDictionary[oneGrams[index]] = index		# This assigns the color values to each token
 
 
 			stack = []
@@ -31,8 +30,8 @@ class ColorAssignment:
 					tokens = currNode.data.split(' ')				# Check for individual tokens
 					
 					for token in tokens:							
-						if(token in colorDictionary):
-							currNode.color.append(colorDictionary[token])	# Assign colors
+						if(token in self.colorDictionary):
+							currNode.color.append(self.colorDictionary[token])	# Assign colors
 
 					for childNodes in currNode.children:			# Add children to the stack
 						stack.append(childNodes)
