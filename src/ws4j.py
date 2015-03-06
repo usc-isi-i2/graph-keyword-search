7612e17ef825b92a10da1b88fff19c41b9191fe8
bhatshreenidhi <bhatshreenidhi@gmail.com>
2015-03-06 15:15:16 -0800

    updated pilot program to get the score

--- src/ws4j.py
+++ src/ws4j.py
@@ -1,6 +1,7 @@
 import urllib.request
 import sys
-
+import json
+from wordSimilarity import *
 
 word1 = input(" Enter the word1 : ")
 word2 = input(" Enter the word2 : ")
@@ -13,4 +14,9 @@ request = urllib.request.Request(url)
 request.add_header('Accept', 'application/json')
 response = urllib.request.urlopen(request)
 
-print (response.read().decode('utf-8'))
+responseStr = response.read().decode('utf-8')
+
+# fetch json from the response
+jsonStr = json.loads(responseStr)
+
+print(jsonStr['result'][0]['score'])
