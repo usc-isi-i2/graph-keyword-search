d5bf4b908b26b9a3e9ed07a7b8e70c1da768c813
bhatshreenidhi <bhatshreenidhi@gmail.com>
2015-03-06 15:14:28 -0800

    Added ws4j similarity measure for determining predicate similarity

--- src/wordSimilarity.py
+++ src/wordSimilarity.py
@@ -1,5 +1,6 @@
 import urllib.request
 import sys
+import json
 
 class WordSimilarity:
 
@@ -12,3 +13,25 @@ class WordSimilarity:
 		print (response.read().decode('utf-8'))
 
 	
+
+	# Use ws4j for predicate similarity
+	# Using threshold as 0.8
+	# If you don't find the word in ws4j, use EasyESA
+	def isPredicateSimilar(word1,word2):
+		url = "http://ws4jdemo.appspot.com/ws4j?measure=wup&args="+word1+"%3A%3A"+word2
+		request = urllib.request.Request(url)
+		request.add_header('Accept', 'application/json')
+		response = urllib.request.urlopen(request)
+		responseStr = response.read().decode('utf-8')
+		# fetch json from the response
+		jsonStr = json.loads(responseStr)
+
+		score = float(jsonStr['result'][0]['score'])
+		if(score>0.8):
+			return True
+		elif(score==-1):
+			return False
+		else:
+			return False
+
+
