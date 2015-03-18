cc8f7813615a15aecbb1ef7a6d601be2be392ba6
bhatshreenidhi <bhatshreenidhi@gmail.com>
2015-03-17 22:58:39 -0700

    URL encoding added

--- src/wordSimilarity.py
+++ src/wordSimilarity.py
@@ -1,37 +1,41 @@
 import urllib.request
 import sys
 import json
+from urllib.parse import quote
 
 class WordSimilarity:
-
-	def getSimilarityValue(word1,word2):
-
-		url = "http://vmdeb20.deri.ie:8890/esaservice?task=esa&term1="+word1+'&term2='+word2
+	def getEasyESAScore(word1,word2):
+		url = "http://vmdeb20.deri.ie:8890/esaservice?task=esa&term1="+quote(word1)+'&term2='+quote(word2)
 		request = urllib.request.Request(url)
 		response = urllib.request.urlopen(request)
+		score = str(response.read().decode('utf-8')).replace('\"','')
+		return float(score)
 
-		print (response.read().decode('utf-8'))
-
-	
-
-	# Use ws4j for predicate similarity
-	# Using threshold as 0.8
-	# If you don't find the word in ws4j, use EasyESA
-	def isPredicateSimilar(word1,word2):
+	def getWs4jScore(word1,word2):
 		url = "http://ws4jdemo.appspot.com/ws4j?measure=wup&args="+word1+"%3A%3A"+word2
 		request = urllib.request.Request(url)
 		request.add_header('Accept', 'application/json')
 		response = urllib.request.urlopen(request)
 		responseStr = response.read().decode('utf-8')
 		# fetch json from the response
-		jsonStr = json.loads(responseStr)
-
-		score = float(jsonStr['result'][0]['score'])
-		if(score>0.8):
-			return score
-		elif(score==-1):
-			return score
+		jsonStr = json.loads(responseStr)	
+		return float(jsonStr['result'][0]['score'])
+	
+	def isPredicateSimilar(word1,word2,option):
+		score = -1
+		if(option!=1):
+			score = WordSimilarity.getEasyESAScore(word1,word2)
+			if(score!=0 or score!=-1):
+				return score
+			else:
+				return -1
 		else:
-			return -1
+			score = WordSimilarity.getEasyESAScore(word1,word2)
+			#score = WordSimilarity.getWs4jScore(word1,word2)
+			#if(score>0.8):
+			if(score!=0 or score!=-1):
+				return score
+			else:
+				return -1
 		
-
+	
