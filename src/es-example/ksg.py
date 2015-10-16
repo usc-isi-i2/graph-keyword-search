83587262b453493c8fcc6418a53b5612fd0d1431
Andrew Philpot <andrew.philpot@gmail.com>
2015-10-15 17:41:25 -0700

    this is a simple main/driver

---
+++ src/es-example/ksg.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import sys
+import argparse
+
+from graph import htGraph
+from kquery import KQuery
+from synonym import SynonymGenerator
+
+g = None
+k = None
+s = None
+
+def main(argv=None):
+    '''this is called if run from command line'''
+    global g,k,s
+    parser = argparse.ArgumentParser()
+    parser.add_argument('terms', nargs='*', default=[], action="append")
+#     parser.add_argument('-t','--type', 
+#                         required=False, 
+#                         help='domain type',
+#                         default='ht')    
+    parser.add_argument('-v','--verbose', required=False, help='verbose', action='store_true')
+    args = parser.parse_args()
+    # nargs generates a list of lists, research this
+    terms = args.terms[0]
+    g = htGraph()
+    print("Terms: {}".format(terms))
+    s = SynonymGenerator()
+    k = KQuery(terms, g, s)
+    return k
+
+# call main() if this is run as standalone
+if __name__ == "__main__":
+    sys.exit(main())
