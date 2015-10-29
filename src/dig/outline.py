c44ebf3fce38ad7bb8599172c1cc2c02cef4368b
Andrew Philpot <andrew.philpot@gmail.com>
2015-10-29 16:06:42 -0700

    path computation

--- src/dig/outline.py
+++ src/dig/outline.py
@@ -8,8 +8,22 @@ except ImportError:
 from pprint import pprint
 from collections import defaultdict
 from util import info
+from networkx import shortest_path
 
 iii = None
+
+def pathFromRoot(graph, cand, node, root):
+    nodes = shortest_path(graph, root, node)
+    pathComponents = [root]
+    waypoints = nodes[0:-1]
+    for (f,t) in zip(waypoints, waypoints[1:]):
+        pathComponents.append(graph.labelInGraph((f,t)) or "missing")
+    # pathComponents.append(nodes[-1].upper())
+    # terminus is a leaf node, named after class plus relation
+    # we only want the relation
+    pathComponents.append(node.split('.')[-1])  
+    path = ".".join(pathComponents)
+    return path
 
 class Outline(object):
     def __init__(self, graph, subgraph, query, root, **kwargs):
@@ -31,16 +45,19 @@ class Outline(object):
         for a in self.query.ngrams.values():
             for cand in a["candidates"]:
                 if cand.referentType == 'node':
-                    print("node:")
-                    # print(node.explain())
-                    print(cand.explain())
-                    print(cand.synonym.explain())
-                    must.append(cand.binding())
-                    nodesMentioned.append(cand.binding())
-                    # required[truenodeDesig(cand.referent)].append(cand)
+                    node = cand.referent
+                    if self.graph.isLeaf(node):
+                        must.append( {"path": pathFromRoot(self.graph, cand, node, self.root),
+                                      "matchType": "direct" if cand.candidateType == "direct" else "fuzzy",
+                                      "operands": [cand.referent, cand.content],
+                                      "explanation": cand} )
+                    else:
+                        nodesMentioned.append(cand.binding())
                     for w in a["words"]:
                         touches[w].add(cand)
                 elif cand.referentType == 'edge':
+                    edge = cand.referent
+                    print("edge: {}".format("leaf" if self.graph.isLeaf(edge) else "nonleaf"))
                     edgesMentioned.append(cand.binding())
                     # required[truenodeDesig(cand.referent)].append(cand)
                     for w in a["words"]:
