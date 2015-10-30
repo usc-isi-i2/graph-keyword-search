7536c183c6ed4fafd51984855229b41ca948b592
Andrew Philpot <andrew.philpot@gmail.com>
2015-10-30 08:52:51 -0700

    interpret Outline.explain to include _explanation, else drop it

--- src/dig/outline.py
+++ src/dig/outline.py
@@ -1,13 +1,7 @@
 #!/usr/bin/env python
 
 import sys
-try:
-    from StringIO import StringIO
-except ImportError:
-    from io import StringIO
-from pprint import pprint
 from collections import defaultdict
-from util import info
 from networkx import shortest_path
 import json
 
@@ -27,12 +21,13 @@ def pathFromRoot(graph, cand, node, root):
     return path
 
 class Outline(object):
-    def __init__(self, graph, subgraph, query, root, verbose=False, **kwargs):
+    def __init__(self, graph, subgraph, query, root, verbose=False, explain=False, **kwargs):
         self.graph = graph
         self.subgraph = subgraph
         self.query = query
         self.root = root
         self.verbose = verbose
+        self.explain = explain
 
     def intermediate(self):
         global iii
@@ -50,37 +45,54 @@ class Outline(object):
                     node = cand.referent
                     if self.graph.isLeaf(node):
                         # Leaf node corresponds to an equality/fuzzy relation constraint 
-                        must.append( {"path": pathFromRoot(self.graph, cand, node, self.root),
-                                      "matchType": "direct" if cand.candidateType == "direct" else "inferred",
-                                      # "operands": [cand.referent, cand.content],
-                                      "className": cand.referent.split('.')[0],
-                                      "relationName": cand.referent.split('.')[1],
-                                      "value": cand.content,
-                                      "_explanation": cand.explain() } )
+                        m = {"path": pathFromRoot(self.graph, cand, node, self.root),
+                             "matchType": "direct" if cand.candidateType == "direct" else "inferred",
+                             # "operands": [cand.referent, cand.content],
+                             "className": cand.referent.split('.')[0],
+                             "relationName": cand.referent.split('.')[1],
+                             "value": cand.content}
+                        if self.explain:
+                            m["_explanation"] = cand.explain()
+                        must.append(m)
                     else:
                         # Other node corresponds to mention of a class (e.g., the word 'seller' is mentioned)
-                        classesMentioned.append( {"className": self.graph.labelInGraph(node),
-                                                  "_explanation": cand.explain() })
+                        m = {"className": self.graph.labelInGraph(node)}
+                        if self.explain:
+                            m["_explanation"] = cand.explain()
+                        classesMentioned.append(m)
                     # Record (possibly partial) coverage of query terms
                     for w in a["words"]:
-                        touches[w].append(cand.explain())
+                        t = {"term": w,
+                             "foundIn": "node"}
+                        if self.explain:
+                            t["_explanation"] = cand.explain()
+                        touches[w].append(t)
                 elif cand.referentType == 'edge':
                     edge = cand.referent
                     # Edge match corresponds to mention of an edge
                     # May or may not correspond to relation constraint on that edge
                     # In future, this might mean we want result to include its class
-                    relationsMentioned.append( {"className": self.graph.labelInGraph(edge[0]),
-                                                "relationName": self.graph.labelInGraph(edge),
-                                                "_explanation": cand.explain() } )
+                    m = {"className": self.graph.labelInGraph(edge[0]),
+                         "relationName": self.graph.labelInGraph(edge)}
+                    if self.explain:
+                        m["_explanation"] = cand.explain()
+                    relationsMentioned.append(m)
                     # Record (possibly partial) coverage of query terms
                     for w in a["words"]:
-                        touches[w].append(cand.explain())
+                        t = {"term": w,
+                             "foundIn": "edge"}
+                        if self.explain:
+                            t["_explanation"] = cand.explain()
+                        touches[w].append(t)
         # Any terms never covered are now free-text matches
         for term in self.query.terms:
             if not touches[term]:
-                should.append( {"matchType": "free",
-                                "operands": [term],
-                                "_explanation": "{} uninterpretable".format(term) })
+                s = {"matchType": "free",
+                     "operands": [term]}
+                if self.explain:
+                    s["_explanation"] = "{} uninterpretable".format(term)
+                should.append(s)
+                                
         i["touches"] = touches
         i["relationsMentioned"] = relationsMentioned
         i["classesMentioned"] = classesMentioned
