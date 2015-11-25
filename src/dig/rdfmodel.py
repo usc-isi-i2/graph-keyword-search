9e7e3a1e1c2af590ee05ad5eef1df8c8eea4869a
Andrew Philpot <andrew.philpot@gmail.com>
2015-11-24 16:38:14 -0800

    loads relevant ontologies: are these needed?

--- src/dig/rdfmodel.py
+++ src/dig/rdfmodel.py
@@ -1,16 +1,29 @@
+import os
 import rdflib
+from rdflib.util import guess_format
 
-g = rdflib.Graph()
-# result = g.parse("http://www.w3.org/People/Berners-Lee/card")
-result1 = g.parse("data/ontology/memex-ontology.ttl", format="turtle")
-result2 = g.parse("data/ontology/rdf-schema.owl", format="xml")
-result3 = g.parse("data/ontology/skos.rdf", format="xml")
-
-print("graph has %s statements." % len(g))
-# prints graph has 79 statements.
-
-for subj, pred, obj in g:
-    if (subj, pred, obj) not in g:
-        raise Exception("It better be!")
-
-s = g.serialize(format='n3', destination="/tmp/g.n3")
+def loadModel(*pathnames):
+    """Not general"""
+    d = "data/ontology"
+    if not pathnames:
+        pathnames = [os.path.join(d, f) for f in os.listdir(d)]
+    g = rdflib.Graph()
+    for pathname in pathnames:
+        fmt = guess_format(pathname)
+        result = g.parse(pathname, format=fmt)
+    return g
+#               
+#               
+# # result = g.parse("http://www.w3.org/People/Berners-Lee/card")
+# result1 = g.parse("data/ontology/memex-ontology.ttl", format="turtle")
+# result2 = g.parse("data/ontology/rdf-schema.owl", format="xml")
+# result3 = g.parse("data/ontology/skos.rdf", format="xml")
+# 
+# print("graph has %s statements." % len(g))
+# # prints graph has 79 statements.
+# 
+# for subj, pred, obj in g:
+#     if (subj, pred, obj) not in g:
+#         raise Exception("It better be!")
+# 
+# s = g.serialize(format='n3', destination="/tmp/g.n3")
